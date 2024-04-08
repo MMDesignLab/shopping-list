84be3c6de9c036e718ebf3eb9369c05ccf2e8467
Monica Mendez <smendez22@cnm.edu>
2024-04-08 07:40:31 -0600

    added index.html and script.js files
    added item-form-container and unordered list in index.html
    
    added window.onload and initShoppingList function
    
    added delete item button
    
    created random number function and set number
    also attached an event to the delete item button

--- script.js
+++ script.js
@@ -23,17 +23,32 @@ function handleItemForm(event, formRef) {
 function addItemToShoppingList(){
     let itemName = document.getElementById("item-name");
     let itemAmount = document.getElementById("item-amount");
+    let id = getRandomInt(0,10000000); //create an id and then call our getrandomint function
 
-    let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+//creates list item html and appends to page
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
     console.log("Item HTML: ", itemHtml);
     let itemListRef = document.getElementById("shopping-list");
     itemListRef.insertAdjacentHTML("afterend", itemHtml)
 }
+//attach an event to the button
+function setDeleteButtonEvent(id){
+    let deleteButton = document.getElementById("button"+id);
+    deleteButton.addEventListener("click", () => {
+        console.log("DeleteButton Works") //to test create consolelog
+    });
+}
 
-function createListItemHtml(itemName, itemAmount) {
-    return `<li>
+function createListItemHtml(itemName, itemAmount, id) {
+    return `<li id="item${id}"> //add the id to the list item and button
                 ${itemName} - ${itemAmount}
-                <button type="button">Delete Item</button>
+                <button type="button${id}">Delete Item</button>
             </li>`;
 
+}
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled)) + minCeiled; // The maximum is exclusive and the minimum is inclusive
 }
