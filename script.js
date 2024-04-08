9b92f48590b8ee7d186ad33c18a6204c900d08d4
Monica Mendez <smendez22@cnm.edu>
2024-04-08 08:09:38 -0600

    added index.html and script.js files
    added item-form-container and unordered list in index.html
    
    added window.onload and initShoppingList function
    
    added delete item button
    
    created random number function and set number
    also attached an event to the delete item button
    
    add formRef reset to handleItemForm to remove historical data
    
    call the function to delete items setDeleteButtonEvent
    
    added the function that removes the list item removeListItem and called the function from setDeleteButtonEvent

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function setDeleteButtonEvent(id){
 function createListItemHtml(itemName, itemAmount, id) {
     return `<li id="item${id}"> 
                 ${itemName} - ${itemAmount}
-                <button type="button${id}">Delete Item</button>
+                <button id="button${id}" type="button">Delete Item</button>
             </li>`;
 
 }
